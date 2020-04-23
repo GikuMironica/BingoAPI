c6b361b55b526780bf18937b5a9940ecb5f16ba1
mironicagicu <mironicagicu@gmail.com>
2020-04-23 16:03:09 +0200

    Sample Web Client for facebook auth implemented

---
+++ Bingo.TestWebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bingo.TestWebClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
