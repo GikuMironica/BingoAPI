7efdca5b7405c1a6ae50c79ed346c05ffdb8cefa
mironicagicu <mironicagicu@gmail.com>
2020-04-23 16:03:27 +0200

    Facebook authentication implemented

---
+++ BingoAPI/ExternalLogin/FacebookUserInfoResult.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BingoAPI.ExternalLogin
+{
+    public class FacebookUserInfoResult
+    {
+        [JsonProperty("first_anem")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("picture")]
+        public FacebookPicture Picture { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+
+    public class FacebookPicture
+    {
+        [JsonProperty("data")]
+        public FacebookPictureData Data { get; set; }
+    }
+
+    public class FacebookPictureData
+    {
+        [JsonProperty("height")]
+        public long Heigth { get; set; }
+
+        [JsonProperty("is_silhouette")]
+        public bool IsSilhouette { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+
+
+    }
+}
