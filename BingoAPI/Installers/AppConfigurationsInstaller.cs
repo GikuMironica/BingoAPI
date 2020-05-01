ec8464628a863e888ef1fc42ec29a87ed939d555
mironicagicu <mironicagicu@gmail.com>
2020-05-01 22:43:08 +0200

    Implement Email messager

---
+++ BingoAPI/Installers/AppConfigurationsInstaller.cs
@@ -0,0 +1,20 @@
+﻿using BingoAPI.Options;
+using BingoAPI.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BingoAPI.Installers
+{
+    public class AppConfigurationsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ApplicationEmailSettings>(configuration.GetSection("ApplicationEmail"));
+            services.AddSingleton<IEmailService, EmailService>();
+        }
+    }
+}
