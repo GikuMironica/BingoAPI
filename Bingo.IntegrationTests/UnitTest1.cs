7a061754fbdba947399aa3c1e6580b9909f096d9
mironicagicu <mironicagicu@gmail.com>
2020-06-14 23:34:47 +0200

    Test project added

---
+++ Bingo.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Bingo.IntegrationTests
+{
+    public class UnitTest1
+    {
+
+        private readonly HttpClient client;
+        public UnitTest1()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
