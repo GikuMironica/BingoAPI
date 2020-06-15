50e28d80672a3dad6cd53676b13ad4bb31b285a5
mironicagicu <mironicagicu@gmail.com>
2020-06-15 17:42:20 +0200

    Test project configured

---
+++ Bingo.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,63 @@
+﻿using Bingo.Contracts.V1;
+using Bingo.Contracts.V1.Requests.Identity;
+using Bingo.Contracts.V1.Responses.Identity;
+using BingoAPI;
+using BingoAPI.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo.IntegrationTests
+{
+    
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDB");
+                        });
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var request = new UserRegistrationRequest
+            {
+                Email = "test@integration.com",
+                Password = "SomePass123!"
+            };
+            var param = JsonConvert.SerializeObject(request);
+            var data = new StringContent(param, Encoding.UTF8, "application/json");
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, data);
+            
+
+            var registrationResponse = await response.Content.ReadFromJsonAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+    }
+}
---
+++ Bingo.IntegrationTests/PostControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Bingo.Contracts.V1;
+using BingoAPI.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bingo.IntegrationTests
+{
+    public class PostControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task Get()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("postId", "1"));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadFromJsonAsync<Post>()).Should().BeNull();
+        }
+    }
+}
