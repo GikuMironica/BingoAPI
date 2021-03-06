3384413858d395107b81c29e368dc6367bb34511
mironicagicu <mironicagicu@gmail.com>
2021-03-06 13:25:54 +0100

    Error Db Migrations

---
+++ BingoAPI/Migrations/ErrorData/20210306122116_ServeName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BingoAPI.Migrations.ErrorData
+{
+    public partial class ServeName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Server",
+                table: "ErrorLogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Server",
+                table: "ErrorLogs");
+        }
+    }
+}
