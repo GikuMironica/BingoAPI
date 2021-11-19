381ab5dc521336a91db1028a00293a21e0d7e23f
mironicagicu <mironicagicu@gmail.com>
2021-11-19 22:07:29 +0100

    Fix rating

--- BingoAPI/Models/SqlRepository/IRatingRepository.cs
+++ BingoAPI/Models/SqlRepository/IRatingRepository.cs
@@ -7,7 +7,7 @@ namespace BingoAPI.Models.SqlRepository
 {
     public interface IRatingRepository : IRepository<Rating>
     {
-        public Task<bool> HasAlreadyRatedAsync(string requesterId, string hostId, int postId);
+        public Task<bool> HasAlreadyRatedAsync(string requesterId, int postId);
         public Task<bool> IsRatingOwnerAsync(string requesterId, int ratingId);
         public Task<List<Rating>> GetAllAsync(string userId);
         public Task<double> GetUserRating(string userId);
